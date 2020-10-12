abfcaef6b6c1a56f9f22ee246bce278fbeba1749
kosukemakita <makitakosuke0114@gmail.com>
2020-10-12 21:38:50 +0900

    タイマー機能の追加

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var timerLabel: UILabel!
     
+    var timer: Timer!
      var timer_sec: Float = 0
     
     override func viewDidLoad() {
@@ -23,6 +24,25 @@ class ViewController: UIViewController {
     @objc func updateTimer(_ timer: Timer) {
     self.timer_sec += 0.1
     self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+    }
+    
+    @IBAction func startTimer(_ sender: Any) {
+        if self.timer == nil{
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+        }
+    }
+    @IBAction func pauseTimer(_ sender: Any) {
+        if timer != nil{
+        self.timer.invalidate()
+    }
+    }
+    @IBAction func resetTimer(_ sender: Any) {
+        self.timer_sec = 0
+        self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+        if self.timer != nil {
+            self.timer.invalidate()   // タイマーを停止する
+            self.timer = nil          // startTimer() の self.timer == nil で判断するために、 self.timer = nil としておく
+        }
     }
 }
 
