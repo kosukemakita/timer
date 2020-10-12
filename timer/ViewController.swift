1b19c55e4a0f83bc6a103f84abb1483175f22827
kosukemakita <makitakosuke0114@gmail.com>
2020-10-12 21:29:46 +0900

    ラベル、再生、一時停止、リセットボタンの追加

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+     var timer_sec: Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // タイマーの作成、始動
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
     }
-
-
+    @objc func updateTimer(_ timer: Timer) {
+    self.timer_sec += 0.1
+    self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+    }
 }
 
